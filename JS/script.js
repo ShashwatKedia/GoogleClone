e77bb6cf04a58048e7579369c6f121bc65e31a57
ShashwatKedia <142137555+ShashwatKedia@users.noreply.github.com>
2023-08-16 12:32:58 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 const searchInput = document.querySelector("#search-input");
 
 searchInput.addEventListener("keydown", function(event) {
-    if(event.code === "Enter" || event.code === "Return" || event.code === "Search") {
+    if(event.code === "Enter" || event.code === "Tab" || event.code === "Search") {
         search();
     }
      else{
