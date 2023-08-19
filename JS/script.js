c2438f2526b88d4a9265c5ecf261b616badc34ff
ShashwatKedia <142137555+ShashwatKedia@users.noreply.github.com>
2023-08-19 10:06:49 +0530

    Updated script.js

--- JS/script.js
+++ JS/script.js
@@ -5,17 +5,16 @@ searchInput.addEventListener("keydown", function(event) {
     if(code === "Enter") {
         search();
     }
-     else{
-        prompt("Code is :"+code);
+      else if(event.keyCode === 13){
+        search();
     }
 });
 
 function search() {
     const input = searchInput.value;
 
-    window.location.href = "https://www.google.com/search?q=" + input + "&rlz=1C5CHFA_enNZ948NZ948&oq=" + input + "&aqs=chrome.0.69i59l2j46i175i199i433j46i199i291i433j46j0i433j0j69i60.875j0j9&sourceid=chrome&ie=UTF-8"
+    window.location.href = "https://www.google.com/search?client=safari&rls=en&q="+input+"&ie=UTF-8&oe=UTF-8"; 
 }
-
 
 searchInput.addEventListener("keypress", function(event){
     if(event.keyCode === 13) alert('Enter');
