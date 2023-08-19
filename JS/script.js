b7555043abd4c2921c0c97902c0557c9c34c8d2b
ShashwatKedia <142137555+ShashwatKedia@users.noreply.github.com>
2023-08-19 11:28:54 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -12,6 +12,7 @@ searchInput.addEventListener("keydown", function(event) {
 
 function search() {
     const input = searchInput.value;
+     searchInput.value = "";
     window.location.href = "https://www.google.com/search?client=safari&rls=en&q="+input+"&ie=UTF-8&oe=UTF-8"; 
-    searchInput.value = "";
+   
 }
